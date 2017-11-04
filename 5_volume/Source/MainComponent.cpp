632ddd3e1fdde20c91fb883919c574eb45576f85
Tom Johnson <tom@tmjohnson.co.uk>
2017-11-04 21:14:34 +0000

    Added code for fifth post

---
+++ 5_volume/Source/MainComponent.cpp
@@ -0,0 +1,99 @@
+/*
+  ==============================================================================
+
+    This file was auto-generated!
+
+  ==============================================================================
+*/
+
+#include "../JuceLibraryCode/JuceHeader.h"
+#include "Control.h"
+#include "SerialConnection.h"
+#include "Volume.h"
+#include "AudioEffect.h"
+
+//==============================================================================
+/*
+    This component lives inside our window, and this is where you should put all
+    your controls and content.
+*/
+class MainContentComponent   : public AudioAppComponent
+{
+public:
+    //==============================================================================
+  MainContentComponent() : conn(),
+                           effects{}
+    {
+        setSize (800, 600);
+
+        auto volume = std::make_shared<Volume>();
+        conn.registerEffect(volume);
+        effects.push_back(std::move(volume));
+
+        conn.startThread();
+        // specify the number of input and output channels that we want to open
+        setAudioChannels (2, 2);
+    }
+
+    ~MainContentComponent()
+    {
+        shutdownAudio();
+    }
+
+    //==============================================================================
+    void prepareToPlay (int samplesPerBlockExpected, double sampleRate) override
+    {
+        // This function will be called when the audio device is started, or when
+        // its settings (i.e. sample rate, block size, etc) are changed.
+
+        // You can use this function to initialise any resources you might need,
+        // but be careful - it will be called on the audio thread, not the GUI thread.
+
+        // For more details, see the help for AudioProcessor::prepareToPlay()
+    }
+
+    void getNextAudioBlock (const AudioSourceChannelInfo& bufferToFill) override
+    {
+        for (int i = 0; i < effects.size(); i++)
+        {
+            auto&& effect = effects[i];
+            effect->processBlock(bufferToFill);
+        }
+    }
+
+    void releaseResources() override
+    {
+        // This will be called when the audio device stops, or when it is being
+        // restarted due to a setting change.
+
+        // For more details, see the help for AudioProcessor::releaseResources()
+    }
+
+    //==============================================================================
+    void paint (Graphics& g) override
+    {
+        // (Our component is opaque, so we must completely fill the background with a solid colour)
+        g.fillAll (getLookAndFeel().findColour (ResizableWindow::backgroundColourId));
+
+        // You can add your drawing code here!
+    }
+
+    void resized() override
+    {
+        // This is called when the MainContentComponent is resized.
+        // If you add any child components, this is where you should
+        // update their positions.
+    }
+
+private:
+    //==============================================================================
+
+    // Your private member variables go here...
+
+    SerialConnection conn;
+    std::vector<std::shared_ptr<AudioEffect>> effects;
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (MainContentComponent)
+};
+
+// (This function is called by the app startup code to create our main component)
+Component* createMainContentComponent()     { return new MainContentComponent(); }
--- README.md
+++ README.md
@@ -4,14 +4,20 @@ This repo contains code examples for the DJ Pi blog series, hosted [here](https:
 
 ### 1-2_controls
 
-Contains code for posts [one](https://blog.tmjohnson.co.uk/effects-part-one/) and [two](https://blog.tmjohnson.co.uk/effects-part-two/).
+Contains code for posts [one](https://www.tmjohnson.co.uk/posts/effects-part-one/) and [two](https://www.tmjohnson.co.uk/posts/effects-part-two/).
 
 Covers reading analogue inputs into an Arduino and transmitting them to a C++ (JUCE) application running on a Raspberry Pi.
 
 ### 3-4_controls
 
-Contains code for posts [three](https://blog.tmjohnson.co.uk/effects-part-three/) and [four](https://blog.tmjohnson.co.uk/effects-part-four/).
+Contains code for posts [three](https://www.tmjohnson.co.uk/posts/effects-part-three/) and [four](https://www.tmjohnson.co.uk/posts/effects-part-four/).
 
 The Arduino code covers developing a binary message format and reading multiple analogue inputs.
 
 The Pi code using event emitters and broadcasters to transmit values to multiple components.
+
+### 5_controls
+
+Contains code for post [five](https://www.tmjohnson.co.uk/posts/effects-part-five/).
+
+In which the author creates sound.
